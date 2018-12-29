84fa8d57e1f49cd20b005261cd606ee01dd2497f
JPBlancoDB <jpblancodb@gmail.com>
2018-12-29 18:47:58 +0100

    Refactor clean-blog template to have split layout

--- blog/client/urls.py
+++ blog/client/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post', views.post, name='post'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
